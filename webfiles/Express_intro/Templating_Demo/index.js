eedae0bb01525d2bfde9722db2fee773b705231a
Fortunize <96587252+Fortunize@users.noreply.github.com>
2022-06-09 14:59:34 +0100

    RestFul Api

--- webfiles/Express_intro/Templating_Demo/index.js
+++ webfiles/Express_intro/Templating_Demo/index.js
@@ -11,6 +11,7 @@ app.set('views', path.join(__dirname, '/views'));
 app.get('/', (req, res) => {
     res.render('home.ejs');
 })
+
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
     const data = redditData[subreddit];
@@ -26,6 +27,7 @@ app.get('/c', (req, res) => {
     const cats = ['Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston', 'Soccer']
     res.render('cats', { cats: cats });
 })
+
 app.get('/:rand', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1;
     res.render('random', { num: num });
