f4af66367aa87ff82935995d749462d40543de5c
Fortunize <96587252+Fortunize@users.noreply.github.com>
2022-06-25 22:41:02 +0100

    yelpcamp

--- webfiles/YelpCamp/app.js
+++ webfiles/YelpCamp/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+
+});
+
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-    res.send('Hello From Yelp Camp!');
+    res.render('home');
 })
 
 app.listen(3000, () => {
